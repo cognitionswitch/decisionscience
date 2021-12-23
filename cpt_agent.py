ef6e3363dc87b682853c7e23be32d9224ee366b6
unknown <carlosacabrera@gmail.com>
2021-12-22 22:22:31 -0500

    Created BaseConstructor class for recording constructor inputs; cleaned up __repr__ method

--- cpt_agent.py
+++ cpt_agent.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import datetime as dt
 import numpy as np
 
-class PTAgent:
-    """Generate an Prospect Theory agent that expresses preferences between two lotteries 
-    (ie., gambles) according to principles of Prospect Theory (1979). 
+
+class BaseConstructor:
+    
+    def __init__(self, *args):
+        constructor_vals = locals()['args']
+        constructor_parms = [k for k in self.__init__.__code__.co_varnames if k != 'self']
+        self.init_args = {k:v for k,v in zip(constructor_parms, constructor_vals)}
+        
+        for name, val in self.init_args.items():
+            setattr(self, name, val)
+            
+
+class PTAgent(BaseConstructor):
+    """Generate a Prospect Theory agent that expresses preferences between two lotteries according Kahneman & Tversky (1979). 
     
     PTAgent expresses preference between lotteries by implementing value and 
     probability weighting functions following Tversky & Kahneman, 
@@ -15,92 +27,145 @@ class PTAgent:
     The functional form of the weighting function is taken from this website:
     https://sites.duke.edu/econ206_01_s2011/files/2011/04/39b-Prospect-Theory-Kahnemann-Tversky_final2-1.pdf
     
-    Parameters
-    ------------
-    alpha: float {a | 0 <= a <= 1}
-        - parameter controlling the rate of change in value over gains
-    beta (optional): float {b | 0 <= b <= 1}, default=None
-        - parameter controlling the rate of change in value over losses
-    lambda_: float {l| l > 1}
-        - parameter controlling the steepness of the value function over losses relative to gains 
-    delta: float {d| 0 <= d <= 1}
-        - weighting function parameter controlling the shape of the transformation of probability
-        
     Attributes
     ------------
-    parameters: dictionary
-        - a mapping of value function and weighting function parameter names (keys) and parameter inputs (values) 
+    parameters: dict
+        - a mapping of value function and weighting function parameter names (keys) and parameter 
+        inputs (values) 
     _value_fn: function
         - input floating point representing outcome value
-        - output calculation of value function based on input, alpha, beta (optional), and lambda_ parameters
+        - output calculation of value function based on input, alpha, beta (optional), and lambda_ 
+        parameters
     _gain_wt_fn: function
         - input floating point representing probability
-        - output calculation of weighting function for probabilities associated with non-negative outcomes 
+        - output calculation of weighting function for probabilities associated with non-negative 
+        outcomes 
         based on probability and delta parameter
+        
+    Methods
+    ------------
+    evaluate(lottery:dict, verbose:bool=False)
+    choose(lottery1:dict=None, lottery2:dict=None)
     """
     
     def __init__(self, alpha:float=1, beta:float=None, gamma:float=1, delta:float=None, lambda_:float=1):
+        """
+        Parameters
+        ------------
+        alpha: float {a | 0 <= a <= 1}, default=1
+            - controls the rate of change in value over gains
+        beta (optional): float {b | 0 <= b <= 1}, default=None
+            - controls the rate of change in value over losses
+        lambda_: float {l| l > 1}, default=1
+            - controls the steepness of the value function over losses relative to gains 
+        delta: float {d| 0 <= d <= 1}, default=None
+            - a weighting function parameter controlling the shape of the transformation of probability
+        """
+        
+        super().__init__(alpha, beta, gamma, delta, lambda_)
         
         beta = beta or alpha
         delta = delta or gamma
         
-        # range checks
-        assert self._zero_one_bound(alpha, beta, gamma, delta),\
-        'alpha, gamma (and optional beta, delta) parameters must lie within 0 and 1 inclusive'
-        assert lambda_ >= 1,\
-        'lambda_ parameter must be be greater than or equal to 1'
+        self.dt = dt.datetime.now()
         
-        self.parameters = {'alpha':alpha, 
-                           'beta':beta, 
-                           'gamma':gamma,
-                           'delta':delta,
-                           'lambda':lambda_
-                          }
+        self.parameters = {
+            'alpha':alpha, 
+            'beta':beta, 
+            'gamma':gamma,
+            'delta':delta,
+            'lambda_':lambda_
+        }
         
+        # Error handling for parameters
+        type_check = {k: True for k, v in self.parameters.items() if not isinstance(v, (float, int))}
+        if len(type_check) > 0:
+            raise TypeError('TypeError: ' + ', '.join(self.parameters.keys()) +
+                            ' parameters must be int or float type')
+            
+        zero_one_check = {k: v < 0 or v > 1 for k, v in self.parameters.items() if k != 'lambda_'}
+        if any(zero_one_check.values()):
+            param_keys = ', '.join(k for k in self.parameters.keys() if k != 'lambda_')
+            raise ValueError('ValueError: ' + param_keys + 
+                             ' parameters must lie within 0 and 1 inclusive')
+
+        # value and weighting functions
         self._value_fn = lambda x: x**alpha if x >= 0 else -lambda_*((-x)**beta)
         self._gain_wt_fn = lambda p: np.exp(-(-np.log(p))**gamma)
         self._loss_wt_fn = lambda p: np.exp(-(-np.log(p))**delta)
         
     def __repr__(self):
-        return 'PTAgent(alpha=%r, beta=%r, gamma=%r, delta=%r, lambda_=%r)' \
-    % tuple(self.parameters.values())
+        
+        repr_ls = ['PTAgent('] 
+        for k, v in self.parameters.items():
+            repr_ls.append(k + '=' + str(v) + ',')
+            
+        repr_str = '\n '.join(repr_ls) + '\n)\n @ ' + str(self.dt)
+        return repr_str
+    
+    def _validate_lottery(self, lottery:dict):
 
-    def _zero_one_bound(self, *args)->bool:
-        """Range check on instance parameters.
-        Tests whether all positional inputs fall within 0 and 1 inclusive"""
-        return all([n <= 1 and n >=0 for n in args])
+        try:
+            type_check = {k: not isinstance(v, (list, tuple)) for k, v in lottery.items()}
+            len_check = len(lottery['outcome']) == len(lottery['probability'])
+            zero_one_check = [p.__lt__(0) or p.__gt__(1) for p in lottery['probability']]
+            sum_check = sum(lottery['probability']) <= 1
+            
+            if any(type_check.values()): 
+                raise TypeError('Values in lottery must be iterables of type list or tuple')  
+            elif any(zero_one_check):
+                raise ValueError('ValueError: lottery["probability"] values must lie within 0 and 1 inclusive')
+            assert len_check, ('AssertionError: Length of lottery["outcome"] must match length of lottery["probability"]')
+            assert sum_check, ('AssertionError: Sum of lottery["probability"] must not exceed one')
+        except TypeError as te:
+            print(te)
+        except ValueError as ve:
+            print(ve)
+        except AssertionError as ae:
+            print(ae)
     
     def evaluate(self, lottery:dict, verbose:bool=False)->float:
-        """Lottery valuation
+        """Evaluate a lottery.
+        
+        Assigns valuation to a lottery.
         
         Parameters
         ----------
-        lottery: dictionary containing two items
-            - key 'outcome': list of real numbers corresponding to outcomes 
-            - key 'probability': list of floats between 0 and 1 
-            Length of list associated with 'probability' must match length of list associated with 'outcome' 
+        lottery: dict 
+            - contains two keys: 'outcome' and 'probability'.
+            - 'outcome' key maps to a list of real numbers corresponding to outcomes. 
+            - 'probability' key maps to a list of floats between 0 and 1.
+            Length of list associated with 'probability' must match length of list associated 
+            with 'outcome'.
         verbose: bool 
-            - True: output list of outcome, probability tuples sorted by outcome; capacities; valuation
+            - True: output list of outcome, probability tuples sorted by outcome; capacities; 
+            valuation
             - False: output valuation
         
-        returns
+        Returns
         ----------
-        valuation (if verbose=False) : float representing overall value of lottery to PTAgent instance
-        dict (if verbose=True):
+        valuation (if verbose=False) 
+            - float representing overall value of lottery to PTAgent instance
+        dict (if verbose=True)
             - valuation : float representing overall value of lottery to PTAgent instance
             - lottery : list of tuples containing outcome, probability pairs sorted by outcome
             - lottery_trans : list of outcomes after value transformation function 
         """
+        
         # consistency checks
-        assert len(lottery['outcome'])==len(lottery['probability']),\
-        'number of probabilities must match number of outcomes'
-        assert all(p >=0 for p in lottery['probability']),\
-        'probabilities must be greater than or equal to 0'       
-        assert sum(lottery['probability']) <= 1, 'sum of probabilities must not exceed one'
+        self._validate_lottery(lottery)
         
         lottery_ls = [(o, p) for o, p in zip(lottery['outcome'], lottery['probability'])]
         lottery_trans_ls = [(self._value_fn(o), self._wt_transform(p, gain=o>=0)) for (o, p) in lottery_ls]
-        valuation = sum(v*w for (v, w) in lottery_trans_ls)
+        
+        if (len(lottery_ls) == 2) & (sum(tup[1] for tup in lottery_ls) < 1) & (all(tup[0] >= 0 for tup in lottery_ls) | all(tup[0] < 0 for tup in lottery_ls)):
+            min_abs_val_idx = np.argmin(abs(tup[0]) < 0 for tup in lottery_ls)
+            max_abs_val_idx = 1 - min_abs_val_idx 
+            low_abs_xpect = lottery_ls[min_abs_val_idx]
+            high_abs_xpect = lottery_ls[max_abs_val_idx]
+            valuation = low_abs_xpect[0] + high_abs_xpect[1]*(high_abs_xpect[0] - low_abs_xpect[0])
+        else: 
+            valuation = sum(v*w for (v, w) in lottery_trans_ls)
         
         if verbose:
             return dict(valuation=valuation, lottery=lottery_ls, lottery_trans=lottery_trans_ls)
@@ -115,7 +180,8 @@ class PTAgent:
         p: float {x: 0 <= x <= 1}
             - probability of a lottery outcome
         gain: bool
-            - boolean signifying whether the outcome associated with probability p is non-negative
+            - boolean signifying whether the outcome associated with probability p is 
+            non-negative
 
         returns: weight transformation of probability p
         """
@@ -134,22 +200,60 @@ class PTAgent:
     
     
     
-
 class CPTAgent(PTAgent):
+    """Generate a Cumulative Prospect Theory agent that expresses preferences between 
+    two lotteries according to Kahneman & Tversky (1992). 
+    
+    Generates preferences between lotteries by implementing value and 
+    probability weighting functions following Tversky & Kahneman, 
+    Advances in Prospect Theory: Cumulative Representation of Uncertainty (1992). 
+    CPTAgent inherits from parent class PTAgent, but overrides .evaluate() method. 
+        
+    Attributes
+    ------------
+    parameters: dictionary
+        - contains parameter names as keys and parameter inputs as values
+    value_fun: function
+        - input floating point representing utility
+        - output calculation of value function based on input, alpha, beta (optional), 
+        and lambda_ parameters
+    weight_fun: function
+        - input floating point representing probability
+        - output calculation of value function based on probability and delta parameter
+    """
     
     def __init__(self, alpha:float=1, beta:float=None, gamma:float=1, delta:float=None, lambda_:float=1):
+        """
+        Parameters
+        ------------
+        alpha: float {a | 0 <= a <= 1}
+            - controls the rate of change in value over gains
+        beta (optional): float {b | 0 <= b <= 1}, default=None
+            - controls the rate of change in value over losses
+        lambda_: float {l| l > 1}
+            - controls the steepness of the value function over losses relative to gains 
+        delta: float {d| 0 <= d <= 1}
+            - a weighting function parameter controlling the shape of the transformation 
+            of probability
+        """
+        
+        super().__init__(alpha, beta, gamma, delta, lambda_)
         
         beta = beta or alpha
         delta = delta or gamma
-        
-        super().__init__(alpha, beta, gamma, delta, lambda_)
         
         # override PTAgent weighting functions with cumulative prospect theory weighting functions (Kahneman & Tversky, 1990)
         self._gain_wt_fn = lambda p: p**gamma/(p**gamma + (1-p)**gamma)**gamma
         self._loss_wt_fn = lambda p: p**delta/(p**delta + (1-p)**delta)**delta
         
     def __repr__(self):
-        return 'CPTAgent(alpha=%r, beta=%r, gamma=%r, delta=%r, lambda_=%r)' % tuple(self.parameters.values())
+        
+        repr_ls = ['CPTAgent('] 
+        for k, v in self.parameters.items():
+            repr_ls.append(k + '=' + str(v) + ',')
+            
+        repr_str = '\n '.join(repr_ls) + '\n)\n @ ' + str(self.dt)
+        return repr_str
     
     def evaluate(self, lottery:dict, verbose:bool=False)->float:
         """Lottery valuation
@@ -175,11 +279,7 @@ class CPTAgent(PTAgent):
         """
         
         # consistency checks
-        assert len(lottery['outcome'])==len(lottery['probability']),\
-        'number of probabilities must match number of outcomes'
-        assert all(p >=0 for p in lottery['probability']),\
-        'probabilities must be greater than or equal to 0'       
-        assert sum(lottery['probability']) <= 1, 'sum of probabilities must not exceed one'
+        self._validate_lottery(lottery)
         
         lottery_ls = [(o, p) for o, p in zip(lottery['outcome'], lottery['probability'])]
         srt_lottery_ls = sorted(lottery_ls, key=lambda x: x[0]) # prospects sorted by outcome
@@ -215,6 +315,7 @@ class CPTAgent(PTAgent):
 
         returns: list of capacities for each outcome-probability pair in sgn_lottery_ls
         """
+        
         ls_len = len(sgn_lottery_ls)
         csum_p_ls = [sum(tup[1] for tup in sgn_lottery_ls[opt:]) for opt in range(0, ls_len)]
         weight_i_ls = [self._wt_transform(p, gain) for p in csum_p_ls]
